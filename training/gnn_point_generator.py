cfc3bbac5f983bf4c863dfd2761c8ed544f1dc09
Kowalski1024 <tomasz_wojnar@hotmail.com>
2025-11-22 18:20:07 +0100

    up

--- training/gnn_point_generator.py
+++ training/gnn_point_generator.py
@@ -426,10 +426,9 @@ class CloudGenerator(nn.Module):
 
         self.tail = nn.Sequential(
             nn.Linear(channels // 2, 3),
-            nn.Tanh(),
         )
-        nn.init.normal_(self.tail[0].weight, std=0.001)
-        nn.init.zeros_(self.tail[0].bias)
+        # nn.init.normal_(self.tail[0].weight, std=0.01)
+        # nn.init.zeros_(self.tail[0].bias)
 
         self.synthetic_block1 = PointGNNConv(128, 128, z_dim)
         self.synthetic_block2 = PointGNNConv(128, 128, z_dim)
