d8d5e0579618bd3e2b14eed8cb4d6339331f9aad
Kowalski1024 <tomasz_wojnar@hotmail.com>
2025-11-22 16:10:26 +0100

    up

--- training/training_loop.py
+++ training/training_loop.py
@@ -323,7 +323,7 @@ def training_loop(
         cur_nimg += batch_size
         batch_idx += 1
 
-        if rank == 0 and (batch_idx - 1) % 2 == 0:
+        if rank == 0 and (batch_idx - 1) % 5 == 0:
             print(f"  kimg {cur_nimg / 1e3:<8.3f} G_loss: {logger.content.get('Loss/G_loss', 0):.4f}  D_loss: {logger.content.get('Loss/D_loss', 0):.4f}  Aniso: {logger.content.get('Geometry/anisotropy', 0):.2f}  Disp: {logger.content.get('Geometry/displacement', 0):.2f}  Dead: {logger.content.get('Geometry/dead_gaussians', 0):.2%}, Logit_Spread: {logger.content.get('Scores/logit_spread', 0):.4f}")
             if 'Gradients/Gmain_grad_norm' in logger.content:
                 print(f"  G_grad: {logger.content['Gradients/Gmain_grad_norm']:.4f}  D_grad: {logger.content.get('Gradients/Dmain_grad_norm', 0):.4f}")
