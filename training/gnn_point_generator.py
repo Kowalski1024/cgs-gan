3c3c4f08efc4daccf53b1c1fe11fcdfe9c7b88fd
Kowalski1024 <tomasz.wojnar@hotmail.com>
2026-01-17 13:30:55 +0100

    up

--- training/gnn_point_generator.py
+++ training/gnn_point_generator.py
@@ -13,9 +13,19 @@ from itertools import pairwise
 from torch_geometric.nn.models.linkx import SparseLinear
 from torch_geometric.utils import spmm
 from dnnlib import EasyDict
-from training.gaussian import GaussianDecoder
+from training.gaussian import GaussianDecoder, trunc_exp
 from training.topology import TopologyFactory
 
+
+SCALE_MAX = 0.02
+SCALE_MIN = 1e-6
+SCALE_INIT = 0.01
+
+def bounded_log_sigmoid(
+    raw: torch.Tensor, log_min: float, log_max: float
+) -> torch.Tensor:
+    span = log_max - log_min
+    return log_min + (span * torch.sigmoid(raw))
 
 def fmm_modulate_linear(
     x: torch.Tensor,
@@ -404,10 +414,60 @@ class SynthesisBlock(nn.Module):
     def __init__(self, in_channels, out_channels, w_dim):
         super().__init__()
         self.geo_conv = PointGNNConv(in_channels, out_channels, w_dim)
-        self.attr_conv = TestBlock(in_channels, out_channels, w_dim)
+        self.attr_conv = BlockTest(in_channels * 2, out_channels * 2, w_dim)
+        self.proj_head = nn.Sequential(
+            nn.Linear(out_channels, out_channels * 2),
+        )
 
-    def forward(self, x, pos, edge_index, w):
-        pass
+    def forward(self, x, y, w, topology):
+        x = self.geo_conv(x, topology.verts, topology.dense_edge_index, w)
+        y = self.attr_conv(y, topology.dense_edge_index, w)
+        y = (self.proj_head(x) + y) / np.sqrt(2.0)
+        return x, y
+
+
+class Decoder(nn.Module):
+    def __init__(self, channel_in, features, w_dim, is_base=False):
+        super().__init__()
+
+        self.channel_in = channel_in
+        self.features = features
+        self.is_base = is_base
+
+        self.channels_out = sum(features.values())
+
+        # self.layer = SynthesisLayer(channel_in, self.channels_out, w_dim, activation=nn.Identity())
+        self.layer = nn.Linear(channel_in, self.channels_out)
+
+        bias = self.layer.bias
+        weight = self.layer.weight
+        if is_base:
+            val_sum = 0
+            scale_log_init = float(np.log(SCALE_INIT).round(2))
+            scale_log_min = float(np.log(SCALE_MIN).round(2))
+            scale_log_max = float(np.log(SCALE_MAX).round(2))
+
+            span = scale_log_max - scale_log_min
+            target_prob = np.clip((scale_log_init - scale_log_min) / span, 1e-4, 1-1e-4)
+            scale_init_bias = float(np.log(target_prob / (1 - target_prob)))
+            opacity_init_bias = float(np.log(0.9 / (1 - 0.9)))
+            scale_init_bias = -5.0
+            for key, val in features.items():
+                if key == "opacity":
+                    bias.data[val_sum : val_sum + val].fill_(opacity_init_bias)
+                elif key == "scale":
+                    bias.data[val_sum : val_sum + val].fill_(scale_init_bias)
+                elif key == "rotation":
+                    bias.data[val_sum : val_sum + val].fill_(0.0)
+                    bias.data[val_sum] = 1.0
+
+                val_sum += val
+        else:
+            bias.data.fill_(0.0)
+            weight.data.fill_(0.0)
+
+    def forward(self, x, w):
+        return self.layer(x)
 
 
 class CloudGenerator(nn.Module):
@@ -415,66 +475,99 @@ class CloudGenerator(nn.Module):
         super().__init__()
         self.z_dim = z_dim
         self.blocks = blocks
+        self.max_level = 6
         self.topology_stack = TopologyFactory.precompute_icosahedron_stack(
-            max_level=5,
+            max_level=self.max_level,
             device='cpu',
         )
+
+        self._scale_log_min = float(np.log(SCALE_MIN).round(2))
+        self._scale_log_max = float(np.log(SCALE_MAX).round(2))
+
+        self.channels = {1: 256, 2: 256, 3: 128, 4: 64, 5: 32, 6: 32}
+        self.blocks = nn.ModuleList()
+        self.xyz_decoders = nn.ModuleList()
+        self.attr_decoders = nn.ModuleList()
+
+        self.features_geo = {"xyz": 3}
+        self.features_attr = {
+            "rotation": 4,
+            "scale": 3,
+            "shs": 3,
+            "opacity": 1,
+        }
+
+        for i in range(2, self.max_level + 1):
+            in_ch = self.channels[i - 1]
+            out_ch = self.channels[i]
+            self.blocks.append(
+                SynthesisBlock(in_ch, out_ch, z_dim)
+            )
+            self.xyz_decoders.append(
+                Decoder(out_ch, self.features_geo, z_dim, is_base=(i==2))
+            )
+            self.attr_decoders.append(
+                Decoder(out_ch * 2, self.features_attr, z_dim, is_base=(i==2))
+            )
+
         self.encoder = GaussianEncoding(
             sigma=10.0,
             input_size=3,
-            encoded_size=128,
+            encoded_size=self.channels[1] // 2,
         )
+
 
         self.proj_head = nn.Sequential(
-            nn.Linear(channels, channels * 2),
+            nn.Linear(self.channels[1], self.channels[1] * 2)
         )
 
-        self.geo_blocks = nn.ModuleList(
-            [
-                PointGNNConv(channels, channels, z_dim)
-                for _ in range(3)
-            ]
-        )
-
-        self.attr_blocks = nn.ModuleList(
-            [
-                BlockTest(channels * 2, channels * 2, z_dim)
-                for _ in range(3)
-            ]
-        )
-
-        self.layer_2 = SynthesisLayer(channels, channels // 2, z_dim, noise=False)
-        self.tail = nn.Sequential(
-            nn.Linear(channels // 2, 3),
-            nn.Tanh(),
-        )
         self.upsample = MeshUpsample()
 
     def forward(self, w):
-        level = 2
-        level_2 = self.topology_stack[level]
-        pos = level_2.verts 
-        edge_index = level_2.dense_edge_index
-        x = self.encoder(pos)  # [N, C]
+        topology = self.topology_stack[1]
+        x = self.encoder(topology.verts)  # [N, C]
         x = x.unsqueeze(0).expand(w.shape[0], -1, -1)  # [B, N, C]
-
         y = self.proj_head(x)
-        for i in range(3):
-            x = self.geo_blocks[i](x, pos, edge_index, w[:, 0])
-            y = self.attr_blocks[i](y, edge_index, w[:, 0])
-            y = (self.proj_head(x) + y) / np.sqrt(2.0)
-            next_level = level + 1
-            topology_next = self.topology_stack[next_level]
-            x = self.upsample(x, topology_next.subdiv_map)
-            y = self.upsample(y, topology_next.subdiv_map)
-            pos = topology_next.verts
-            edge_index = topology_next.dense_edge_index
-            level = next_level
 
-        new_pos = self.layer_2(x, w[:, 0])
-        new_pos = self.tail(new_pos)
+        x_skip = None
+        y_skip = None
+        for i, block in enumerate(self.blocks):
+            x, y = block(x, y, w[:, 0], self.topology_stack[1 + i])
+            new_xyz = self.xyz_decoders[i](x, w[:, 0])  # [B, N, 3]
+            new_attr = self.attr_decoders[i](y, w[:, 0])  # [B, N, C]
+            if x_skip is None and y_skip is None:
+                x_skip = new_xyz
+                y_skip = new_attr
+            else:
+                subdiv_map = self.topology_stack[1 + i].subdiv_map
+                x_skip = self.upsample(x_skip, subdiv_map)
+                y_skip = self.upsample(y_skip, subdiv_map)
+                x_skip = x_skip + new_xyz
+                y_skip = y_skip + new_attr
+            if i + 2 <= self.max_level:
+                subdiv_map = self.topology_stack[1 + i + 1].subdiv_map
+                x = self.upsample(x, subdiv_map)
+                y = self.upsample(y, subdiv_map)
 
-        return new_pos, y
+        raw_splats_vec = torch.cat([x_skip, y_skip], dim=-1)
+        return self.to_splats(raw_splats_vec)
+
+    def to_splats(self, raw_params):
+        B, N, C = raw_params.shape
+        assert C == 14, "Expected 14 channels for splat parameters"
+
+        xyz, rot, scale, color, opac = torch.split(
+            raw_params,
+            [3, 4, 3, 3, 1], # xyz, rotation, scaling, shs, opacity
+            dim=-1,
+        )
+
+        scale = trunc_exp(scale).clamp(min=SCALE_MIN, max=SCALE_MAX)
+        rot = torch.nn.functional.normalize(rot, dim=-1)
+        xyz = torch.tanh(xyz)  # Limit positions to [-1, 1]
+        opac = torch.sigmoid(opac)  # Limit opacity to [0, 1]
+
+        return xyz, scale, rot, color, opac
 
     def _apply(self, fn):
         """
@@ -496,53 +589,8 @@ class PointGenerator(nn.Module):
         super().__init__()
         self.num_pts = options["num_pts"]
         self.point_encoder = CloudGenerator(num_pts=self.num_pts, z_dim=w_dim)
-        self.decoder_scale = GaussianDecoder(
-            {}, 512, hidden_channles=128
-        )
-        # self.decoder_rotation = GaussianDecoder({}, 512, hidden_channles=128)
-        self.decoder_color = GaussianDecoder(
-            {
-                "opacity": 1,
-                "shs": 3,
-                "scaling": 3, 
-                "rotation": 4,
-            },
-            512,
-            hidden_channles=256,
-        )
         self.num_ws = 18
         self.z_dim = w_dim
 
     def forward(self, ws):
-        B = ws.shape[0]
-
-        xyz = torch.empty((B, self.num_pts, 3), device=ws.device)
-        scale = torch.empty((B, self.num_pts, 3), device=ws.device)
-        rotation = torch.empty((B, self.num_pts, 4), device=ws.device)
-        opacity = torch.empty((B, self.num_pts, 1), device=ws.device)
-        color = torch.empty((B, self.num_pts, 3), device=ws.device)
-
-        point_cloud, y = self.point_encoder(ws)
-        # scale_features = self.decoder_scale(
-        #     torch.cat([gaussians_features, pre_feat], dim=-1)
-        # )
-        color_features = self.decoder_color(y)
-        gaussian_model = EasyDict(
-            xyz=point_cloud,
-            # **scale_features,
-            **color_features,
-        )
-
-        xyz = gaussian_model.xyz
-        scale = gaussian_model.scaling
-        rotation = gaussian_model.rotation
-        opacity = gaussian_model.opacity
-        color = gaussian_model.shs
-
-        return (
-            xyz,
-            scale,
-            rotation,
-            color,
-            opacity,
-        )
+        return self.point_encoder(ws)
